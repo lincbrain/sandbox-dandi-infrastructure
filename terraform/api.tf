96b18cd6b5a34541ea8513f3d726e4223a7743c3
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-02-19 11:14:25 -0500

    Upgrade postresql to standard-0

--- terraform/api.tf
+++ terraform/api.tf
@@ -13,6 +13,7 @@ module "api" {
 
   heroku_web_dyno_size    = "standard-1x"
   heroku_worker_dyno_size = "standard-2x"
+  heroku_postgresql_plan  = "standard-0"
   heroku_papertrail_plan  = "ludvig"
 
   django_cors_origin_whitelist       = ["https://gui.dandiarchive.org", "https://gui-beta-dandiarchive-org.netlify.app"]
