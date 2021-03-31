4432df7c0b3d4b9c095a284e41492c06927d4231
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-03-31 15:18:41 -0400

    Use user name instead of internal AWS ID

--- terraform/api.tf
+++ terraform/api.tf
@@ -46,7 +46,7 @@ resource "aws_s3_bucket" "api_dandisets_bucket" {
 }
 
 resource "aws_iam_user_policy" "api_dandisets_bucket" {
-  user   = data.aws_iam_user.api.id
+  user   = data.aws_iam_user.api.user_name
   name   = "dandi-api-dandiset-bucket"
   policy = data.aws_iam_policy_document.api_dandisets_bucket.json
 }
