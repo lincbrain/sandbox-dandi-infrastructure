53681dc5d0a9f38254450647e8b5b0409cf9ee02
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-05-21 16:58:31 -0400

    Downgrade CloudAMQP to Tough Tiger

--- terraform/api.tf
+++ terraform/api.tf
@@ -14,7 +14,7 @@ module "api" {
   heroku_web_dyno_size    = "standard-1x"
   heroku_worker_dyno_size = "standard-2x"
   heroku_postgresql_plan  = "standard-0"
-  heroku_cloudamqp_plan   = "squirrel-1"
+  heroku_cloudamqp_plan   = "tiger"
   heroku_papertrail_plan  = "volmar"
 
   django_cors_origin_whitelist       = ["https://gui.dandiarchive.org", "https://gui-beta-dandiarchive-org.netlify.app"]
