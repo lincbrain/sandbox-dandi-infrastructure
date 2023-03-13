64f63bcc6188a936279c589f14ea639fe36d57c6
Dan LaManna <dan.lamanna@kitware.com>
2023-03-13 17:13:58 -0400

    Remove variable for policy name

--- terraform/sponsored_bucket.tf
+++ terraform/sponsored_bucket.tf
@@ -1,7 +1,6 @@
 module "sponsored_dandiset_bucket" {
   source                                = "./modules/dandiset_bucket"
   bucket_name                           = "dandiarchive"
-  ownership_policy_name                 = "dandi-api-sponsored-bucket"
   public                                = true
   versioning                            = true
   allow_cross_account_heroku_put_object = true
