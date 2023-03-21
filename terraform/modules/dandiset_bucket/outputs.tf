7b4218fbd2941c830987d9fa827f5d5a63929bd3
Dan LaManna <dan.lamanna@kitware.com>
2023-03-21 12:24:06 -0400

    Add log bucket names to django app configuration

--- terraform/modules/dandiset_bucket/outputs.tf
+++ terraform/modules/dandiset_bucket/outputs.tf
@@ -3,6 +3,11 @@ output "bucket_name" {
   description = "The S3 bucket name."
 }
 
+output "log_bucket_name" {
+  value       = aws_s3_bucket.log_bucket.id
+  description = "The S3 log bucket name."
+}
+
 output "bucket_arn" {
   value       = aws_s3_bucket.dandiset_bucket.arn
   description = "The S3 bucket ARN."
