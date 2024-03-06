bc9962d86feb792cb33e678039e15b5fc798e4b1
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-03-05 20:04:10 -0500

    Add import blocks
    We'll want to remove these in a follow-up PR after
    applying.

--- terraform/webdav.tf
+++ terraform/webdav.tf
@@ -12,11 +12,21 @@ resource "heroku_app" "webdav" {
   ]
 }
 
+import {
+  to = heroku_app.webdav
+  id = "dandidav"
+}
+
 resource "heroku_formation" "webdav_heroku_web" {
   app_id   = heroku_app.webdav.id
   type     = "web"
   size     = "basic"
   quantity = 1
+}
+
+import {
+  to = heroku_formation.webdav_heroku_web
+  id = "dandidav:web"
 }
 
 # Enable this feature so that the Rust application can access the git commit hash.
@@ -25,9 +35,19 @@ resource "heroku_app_feature" "webdav_runtime_dyno_metadata" {
   name   = "runtime-dyno-metadata"
 }
 
+import {
+  to = heroku_app_feature.webdav_runtime_dyno_metadata
+  id = "dandidav:runtime-dyno-metadata"
+}
+
 resource "heroku_domain" "webdav" {
   app_id   = heroku_app.webdav.id
   hostname = "webdav.dandiarchive.org"
+}
+
+import {
+  to = heroku_domain.webdav
+  id = "dandidav:webdav.dandiarchive.org"
 }
 
 resource "aws_route53_record" "heroku" {
@@ -36,4 +56,9 @@ resource "aws_route53_record" "heroku" {
   type    = "CNAME"
   ttl     = "300"
   records = [heroku_domain.webdav.cname]
+}
+
+import {
+  to = aws_route53_record.heroku
+  id = "Z02063701JNV8GCOUJIZZ_webdav.dandiarchive.org_CNAME"
 }
