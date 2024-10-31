80d89246c89bff70876270642d46359484983233
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-10-31 12:18:00 -0400

    include default values for now

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "doi_api_password" {
   type        = string
   description = "The password for the Datacite API, used to mint new DOIs during publish."
+  default     = "doi_api_password"
 }
 
 variable "test_doi_api_password" {
   type        = string
   description = "The password for the Datacite Test API, used to mint new DOIs on staging during publish."
+  default     = "doi_api_password"
 }
 
 variable "dev_email" {
   type        = string
   description = "The core developer email list."
+  default     = "dev_email@devemail.com"
 }
