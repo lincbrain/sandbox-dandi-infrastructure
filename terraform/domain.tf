6c9a41e0ca3a9295dc09d901284315a64607dab8
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-11-04 15:02:33 -0500

    Remove terraform that consistently fails upon resource creation

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -2,13 +2,13 @@ resource "aws_route53_zone" "dandi" {
   name = "sandbox-dandi.org"
 }
 
-resource "aws_route53_record" "acm_validation" {
-  zone_id = aws_route53_zone.dandi.zone_id
-  name    = "_cbe41dfe1888c2bb5c157cacc35e1722"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["_46df7ee9a9c17698aedbb737f220c63a.mzlfeqexyx.acm-validations.aws."]
-}
+# resource "aws_route53_record" "acm_validation" {
+#   zone_id = aws_route53_zone.dandi.zone_id
+#   name    = "_cbe41dfe1888c2bb5c157cacc35e1722"
+#   type    = "CNAME"
+#   ttl     = "300"
+#   records = ["_46df7ee9a9c17698aedbb737f220c63a.mzlfeqexyx.acm-validations.aws."]
+# }
 
 resource "aws_route53_record" "gui" {
   zone_id = aws_route53_zone.dandi.zone_id
@@ -34,21 +34,21 @@ resource "aws_route53_record" "www" {
   records = ["sandbox-dandi.github.io"]
 }
 
-resource "aws_route53_record" "email" {
-  zone_id = aws_route53_zone.dandi.zone_id
-  name    = "" # apex
-  type    = "MX"
-  ttl     = "300"
-  records = [
-    "10 mx1.improvmx.com.",
-    "20 mx2.improvmx.com.",
-  ]
-}
-
-resource "aws_route53_record" "email-spf" {
-  zone_id = aws_route53_zone.dandi.zone_id
-  name    = "" # apex
-  type    = "TXT"
-  ttl     = "300"
-  records = ["v=spf1 include:spf.improvmx.com ~all"]
-}
+# resource "aws_route53_record" "email" {
+#   zone_id = aws_route53_zone.dandi.zone_id
+#   name    = "" # apex
+#   type    = "MX"
+#   ttl     = "300"
+#   records = [
+#     "10 mx1.improvmx.com.",
+#     "20 mx2.improvmx.com.",
+#   ]
+# }
+#
+# resource "aws_route53_record" "email-spf" {
+#   zone_id = aws_route53_zone.dandi.zone_id
+#   name    = "" # apex
+#   type    = "TXT"
+#   ttl     = "300"
+#   records = ["v=spf1 include:spf.improvmx.com ~all"]
+# }
